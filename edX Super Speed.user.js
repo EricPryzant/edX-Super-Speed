a0e788001340f5783304ea02d47b64ebe7e475c6
EricPryzant <54600184+EricPryzant@users.noreply.github.com>
2020-08-31 15:00:01 -0500

    Create edX Super Speed.user.js
    Initial commit

---
+++ edX Super Speed.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://ericpryzant.com/
+// @version      0.3
+// @description  Try to take over the world!
+// @author       You
+// @match        https://courses.edx.org/xblock/*
+// @grant        none
+// @run-at document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var vid = document.getElementsByClassName("video")[0];
+    var vid_url = vid.attributes['data-metadata'].value.toString().match("https:\/\/edx-video.net\/.*\.mp4")[0];
+    var vid_title = vid.previousElementSibling.innerText;
+    var zNode = document.createElement ('div');
+    zNode.innerHTML = '<button id="myButton" type="button"><a href=' + vid_url + '>Download ' + vid_title + ' lecture video</a></button>';
+    zNode.setAttribute ('id', 'myContainer');
+    zNode.style = "top:0;right:0;position:absolute;z-index:99999;padding:20px;";
+    document.body.appendChild (zNode);
+
+    var item = document.getElementsByClassName("video-speeds")[0];
+    var new_speed = document.createElement("li");
+    var btn = document.createElement("button");
+    var speed_limit = "2.0";
+    btn.setAttribute("class", "control speed-option");
+    btn.setAttribute("tabindex", -1);
+    btn.setAttribute("aria-pressed", "false");
+    new_speed.appendChild(btn);
+    new_speed.setAttribute("data-speed", speed_limit);
+    new_speed.children[0].innerText = speed_limit + "x";
+    item.prepend(new_speed);
+
+    item = document.getElementsByClassName("video-speeds")[0];
+    new_speed = document.createElement("li");
+    btn = document.createElement("button");
+    speed_limit = "2.5";
+    btn.setAttribute("class", "control speed-option");
+    btn.setAttribute("tabindex", -1);
+    btn.setAttribute("aria-pressed", "false");
+    new_speed.appendChild(btn);
+    new_speed.setAttribute("data-speed", speed_limit);
+    new_speed.children[0].innerText = speed_limit + "x";
+    item.prepend(new_speed);
+
+    item = document.getElementsByClassName("video-speeds")[0];
+    new_speed = document.createElement("li");
+    btn = document.createElement("button");
+    speed_limit = "3.0";
+    btn.setAttribute("class", "control speed-option");
+    btn.setAttribute("tabindex", -1);
+    btn.setAttribute("aria-pressed", "false");
+    new_speed.appendChild(btn);
+    new_speed.setAttribute("data-speed", speed_limit);
+    new_speed.children[0].innerText = speed_limit + "x";
+    item.prepend(new_speed);
+})();
